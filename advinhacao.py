4aa92c4cb7e0d5a97b1359b74ab3d809d9d1479c
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-01-26 22:54:42 -0300

    adicionado diversas tentativas

--- advinhacao.py
+++ advinhacao.py
@@ -3,21 +3,28 @@ print("*   Bem vindo ao jogo de advinhação!   *")
 print("****************************************")
 
 numero_secreto = 42
+numero_total_tentativas = 3
+rodada = 1
 
-chute_usuario_str = input("Digite o seu número: ")
-chute_usuario = int(chute_usuario_str)
 
-print("Você digitou ", chute_usuario)
+while rodada <= numero_total_tentativas:
+    print("Tentativa", rodada, "de", numero_total_tentativas)
+    chute_usuario_str = input("Digite o seu número: ")
+    chute_usuario = int(chute_usuario_str)
 
-acertou = chute_usuario == numero_secreto
-maior = chute_usuario > numero_secreto
+    acertou = chute_usuario == numero_secreto
+    maior = chute_usuario > numero_secreto
 
-if(acertou):
-    print("Você acertou!")
-else:
-    if(maior):
-        print("Você errou! O seu chute foi maior que o número secreto!")
+    if(acertou):
+        print("Você acertou em", rodada, "tentativa(s)")
+        break
     else:
-        print("Você errou! O seu chute foi menor que o número secreto!")
+        if(maior):
+            print("Você errou! O seu chute foi maior que o número secreto!")
+        else:
+            print("Você errou! O seu chute foi menor que o número secreto!")
+
+    rodada += 1
+
 
 print("\nFim do jogo!")
