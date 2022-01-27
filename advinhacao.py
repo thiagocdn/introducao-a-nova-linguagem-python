3e6dad732f21db8e191d2762fe4f432480973550
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-01-26 22:21:41 -0300

    Primeira condicao hardcoded

--- advinhacao.py
+++ advinhacao.py
@@ -1,1 +1,15 @@
-print("Bem vindo!")
+print("****************************************")
+print("*   Bem vindo ao jogo de advinhação!   *")
+print("****************************************")
+
+numero_secreto = 42
+
+chute_usuario_str = input("Digite o seu número: ")
+chute_usuario = int(chute_usuario_str)
+
+print("Você digitou ", chute_usuario)
+
+if (chute_usuario == numero_secreto):
+    print("Você acertou!")
+else:
+    print("Você não acetou :(")
