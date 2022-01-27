e2b6737644d40667153256ff87ef676d335798f2
Thiago Cedran Santos <thiagocdn@gmail.com>
2022-01-26 22:43:35 -0300

    Adicionado dicas

--- advinhacao.py
+++ advinhacao.py
@@ -9,7 +9,15 @@ chute_usuario = int(chute_usuario_str)
 
 print("Você digitou ", chute_usuario)
 
-if (chute_usuario == numero_secreto):
+acertou = chute_usuario == numero_secreto
+maior = chute_usuario > numero_secreto
+
+if(acertou):
     print("Você acertou!")
 else:
-    print("Você não acetou :(")
+    if(maior):
+        print("Você errou! O seu chute foi maior que o número secreto!")
+    else:
+        print("Você errou! O seu chute foi menor que o número secreto!")
+
+print("\nFim do jogo!")
